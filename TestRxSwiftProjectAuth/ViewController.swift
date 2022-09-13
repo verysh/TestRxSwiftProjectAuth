8cace3af917eed4a8100ee0c42792ca7bf6bd800
Владимир Ерышев <v.eryshev@bssys.com>
2022-09-13 21:48:18 +0300

    Initial Commit

---
+++ TestRxSwiftProjectAuth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestRxSwiftProjectAuth
+//
+//  Created by 1 on 13.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
